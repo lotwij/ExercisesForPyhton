8bb70c5ed09b264d49e928945cfeee40eb116421
lotwij <Mormeltje4!>
2019-01-08 21:46:47 +0100

    Created question class

---
+++ Question Quiz.py
@@ -0,0 +1,7 @@
+
+
+Question_prompts = [
+    "What is the coolest city?\n(A) Utrecht\n(B) New York\n(C) Rome\n\n",
+    "What is the best drink?\n(A) Wine\n(B) Diet Coke\n(C) Vodka\n\n",
+    "What is the best time of the year\n(A) My Birthday\n(B) Christmas\n(C) Eastern\n\n",
+]
