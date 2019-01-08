ac563507d3dbaf944f0cc924ec62f213f93ea350
lotwij <Mormeltje4!>
2019-01-08 22:16:11 +0100

    start with question def

--- Question Quiz.py
+++ Question Quiz.py
@@ -1,7 +1,17 @@
-
+from QuestionClass import Question
 
 Question_prompts = [
     "What is the coolest city?\n(A) Utrecht\n(B) New York\n(C) Rome\n\n",
     "What is the best drink?\n(A) Wine\n(B) Diet Coke\n(C) Vodka\n\n",
-    "What is the best time of the year\n(A) My Birthday\n(B) Christmas\n(C) Eastern\n\n",
+    "What is the best time of the year\n(A) My Birthday\n(B) Christmas\n(C) Eastern\n\n"
 ]
+
+questions[
+    Question(Question_prompts[0],"A"),
+    Question(Question_prompts[1], "A"),
+    Question(Question_prompts[2], "B")
+]
+
+def run_test(questions):
+    score = 0
+    for Question in questions
