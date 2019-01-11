5fcd5fa40f5a7611139effc8d6c02d619f25cfe9
lotwij <Mormeltje4!>
2019-01-11 11:18:28 +0100

    Onject Functions & Inheritance

---
+++ ChineseChef.py
@@ -0,0 +1,7 @@
+from ChefInheritance import Chef
+
+class ChineseChef(Chef):
+
+
+    def fried_rice(self):
+        print("The chef makes fried rice")
---
+++ Inheritance.py
@@ -0,0 +1,8 @@
+from ChefInheritance import Chef
+from ChineseChef import ChineseChef
+
+myChef = Chef()
+myChef.make_chicken()
+
+myChef = ChineseChef()
+myChef.make_special_dish()
