2c19c6c21285c2cb8fd68d90b74704380b84c79a
lotwij <Mormeltje4!>
2019-01-11 10:47:31 +0100

    Created question class & quiz

--- Question Quiz.py
+++ Question Quiz.py
@@ -1,17 +1,24 @@
 from QuestionClass import Question
 
-Question_prompts = [
-    "What is the coolest city?\n(A) Utrecht\n(B) New York\n(C) Rome\n\n",
-    "What is the best drink?\n(A) Wine\n(B) Diet Coke\n(C) Vodka\n\n",
-    "What is the best time of the year\n(A) My Birthday\n(B) Christmas\n(C) Eastern\n\n"
+question_prompts = [
+    "What is the coolest city?\n(a) Utrecht\n(b) New York\n(c) Rome\n\n",
+    "What is the best drink?\n(a) Wine\n(b) Diet Coke\n(c) Vodka\n\n",
+    "What is the best time of the year\n(a) My Birthday\n(b) Christmas\n(c) Eastern\n\n"
 ]
 
-questions[
-    Question(Question_prompts[0],"A"),
-    Question(Question_prompts[1], "A"),
-    Question(Question_prompts[2], "B")
+questions = [
+    Question(question_prompts[0], "a"),
+    Question(question_prompts[1], "a"),
+    Question(question_prompts[2], "b")
 ]
 
 def run_test(questions):
     score = 0
-    for Question in questions
+    for question in questions:
+        answer = input(question.prompt)
+        if answer == question.answer:
+            score += 1
+    print("You got " + str(score) + "/" + str(len(questions)) + " correct")
+
+
+run_test(questions)
